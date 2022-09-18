3486d86032b1547166ee6de196f7ad95cd0ebe51
CodeCraft <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-09-18 11:11:30 +0000

    added the work and build dir

--- profiledef.sh
+++ profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_name="osx"
+iso_label="OSX_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
